c69ba21a9039c9035fc3d4891017a03fe591c04a
Didier Brun <db@brundidier.com>
2021-06-08 22:42:09 +0200

    Exercice 1 - python

---
+++ exercice1/index.py
@@ -0,0 +1,17 @@
+import sys
+#
+# Exercice n°1 - Tommy Pasqua dans l'espace
+#
+input = map(int, open("./exercice1/datas/input{}.txt".format(sys.argv[1]), "r").read().split())
+output = int(open("./exercice1/datas/output{}.txt".format(sys.argv[1]), "r").read())
+
+ergoPerUa = 5
+ergolMass, distance = input
+mass = ergolMass + distance * ergoPerUa
+
+print ("-----------------------------")
+print ("Exervice n°1")
+print ("Dataset: {}".format(sys.argv))
+print ("Result: {}".format(mass))
+print ("Solution: {}".format(output))
+print ("-----------------------------")
