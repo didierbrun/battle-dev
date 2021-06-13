1eef32fda173c578cb1802c12ae58595ff5dba56
Didier Brun <db@brundidier.com>
2021-06-13 02:21:58 +0200

    no message

--- exercice6/index.py
+++ exercice6/index.py
@@ -1,8 +1,6 @@
 import sys
 import math
-import copy
 import heapq
-from itertools import combinations
 
 #
 # Exercice n°6 - Expédition minière
@@ -17,6 +15,7 @@ class Planet:
         self.y = y
         self.x = x
         self.value = 0
+        self.distance = math.inf
         for j in range(0, pHeight):
             for i in range(0, pWidth):
                 if planet[j][i] == '*':
@@ -30,11 +29,10 @@ class Graphe:
         self.planets = []
         self.arcs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
         self.findPlanets()
-
-        print("Planets founds : ", len(self.planets))
+        self.distance = None
 
     def applyDiskstra(self, y, x):
-        global width, height, costs, galaxy
+        global width, height, costs, galaxy, pWidth, pHeight
         distance = [[math.inf] * width for i in range(height)]
         visited = [[False] * width for i in range(height)]
         distance[y][x] = 0
@@ -55,7 +53,41 @@ class Graphe:
             
             visited[cy][cx] = True
         
+        self.distance = distance
 
+    def computePlanetScore(self):
+        global pWith, pHeight, width, height, galaxy, structComps
+
+        for p in self.planets:
+            for y in range(0, pHeight):
+                for x in range(0, pWidth):
+                    dy = y + p.y
+                    dx = x + p.x
+                    if galaxy[dy][dx] in structComps:
+                        for arc in self.arcs:
+                            ty = dy + arc[0]
+                            tx = dx + arc[1]
+                            if tx >= 0 and ty >= 0 and tx < width and ty < height:
+                                if self.distance[ty][tx] < p.distance:
+                                    p.distance = self.distance[ty][tx]
+    
+    def resolveSolution(self):
+        global days
+        p = []
+        w = []
+        for planet in self.planets:
+            w.append(planet.distance * 2)
+            p.append(planet.value)
+
+        t = [ [0] * days for i in range(len(p)) ]
+        for i in range(len(p)):
+            for c in range(days):
+                if c >= w[i]:
+                    t[i][c] = max(t[i-1][c], t[i-1][c - w[i]] + p[i])
+                else:
+                    t[i][c] = t[i - 1][c]
+        
+        return t[len(p) -1][days - 1]
 
 
 
@@ -105,13 +137,6 @@ class Graphe:
 
 
 
-
-            
-
-       
-                
-
-
 #
 # Read datas
 #
@@ -121,7 +146,7 @@ output = int(open("./exercice6/datas/output{}.txt".format(sys.argv[1]), "r").rea
 height, width = map(int, input.pop(0).split())
 galaxy = []
 for i in range(0, height):
-    galaxy.append(input.pop(0))
+    galaxy.append(list(input.pop(0)))
 pHeight, pWidth = map(int, input.pop(0).split())
 planet = []
 for i in range(0, pHeight):
@@ -148,8 +173,19 @@ days = int(input.pop(0))
 #
 graphe = Graphe()
 graphe.applyDiskstra(startY, startX)
-#graphe.findBestPlanets()
-#solution = graphe.computeSolution()
+graphe.computePlanetScore()
+solution = graphe.resolveSolution()
+
+tot = 0
+for p in graphe.planets:
+    tot = tot + p.distance
+    #print (p.value, p.distance)
+
+print (days, tot)
+
+# Note filtrer les planêtes par ratio, et vérifier à la main la solution précise
+
+
 
 #
 # Output
@@ -157,6 +193,6 @@ graphe.applyDiskstra(startY, startX)
 print ("-----------------------------")
 print ("Exercice n°6")
 print ("Dataset: {}".format(sys.argv[1]))
-#print ("Result: {}".format(solution))
+print ("Result: {}".format(solution))
 print ("Solution: {}".format(output))
 print ("-----------------------------")
